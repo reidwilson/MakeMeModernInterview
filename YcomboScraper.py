e8f9b5bcd37a27d45680093926cbd3388bb6599f
reidwilson <reid.wilson@vanderbilt.edu>
2017-10-27 12:00:09 -0500

    Last commit

--- YcomboScraper.py
+++ YcomboScraper.py
@@ -8,22 +8,22 @@ driver.quit()
 soup = BeautifulSoup(html, "html.parser")
 tags = soup.select(".storylink")
 count = 0
-tagList = []
-tagsLen = len(tags)
+textList = []
+textListLen = len(tags)
 
-while count != tagsLen:
-    tagList.append(str(tags[count].text))
-    print(tagList[count])
+while count != textListLen:
+    textList.append(str(tags[count].text))
+    print(textList[count])
     count += 1
 
 totalAvg = 0.0
 count = 0
 
-while count != tagsLen:
-    mySplit = tagList[count].split()
+while count != textListLen:
+    mySplit = textList[count].split()
     myCalc = sum(len(cur) for cur in mySplit) / len(mySplit)
     totalAvg = totalAvg + myCalc
     count += 1
 
-totalAvg = totalAvg / tagsLen
+totalAvg = totalAvg / textListLen
 print('\nThe average word length is', totalAvg)

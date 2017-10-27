5b24154884e507584cdc0a390cb7e8b964ae6d78
reidwilson <reid.wilson@vanderbilt.edu>
2017-10-27 11:15:42 -0500

    Near Final Draft Commit

--- YcomboScraper.py
+++ YcomboScraper.py
@@ -5,7 +5,6 @@ driver = webdriver.Chrome()
 driver.get('https://news.ycombinator.com/')
 html = driver.page_source
 driver.quit()
-ostream = open('foo.txt', 'w')
 soup = BeautifulSoup(html, "html.parser")
 #print(soup.prettify()[:])
 letters = soup.select(".storylink")
@@ -14,7 +13,7 @@ wordList = []
 while foo != 30:
     wordList.append(str(letters[foo].text))
     print(wordList[foo])
-    foo = foo + 1
+    foo += 1
 #avgList = []
 totalAvg = 0.0
 foo = 0
@@ -22,7 +21,7 @@ while foo != 30:
     mySplit = wordList[foo].split()
     myCalc = sum(len(cur) for cur in mySplit) / len(mySplit)
     totalAvg = totalAvg + myCalc
-    foo = foo+1
+    foo += 1
     #avgList.append(myCalc)
 totalAvg = totalAvg/30
-print('\nThe total average word length is '+str(totalAvg))
+print('\nThe total average word length is', totalAvg)
